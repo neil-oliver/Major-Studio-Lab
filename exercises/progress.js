b91b0af1f496ec53ae78668da3a9cd7070e22490
EC2 Default User <ec2-user@ip-172-31-28-141.ec2.internal>
2019-09-01 15:16:16 +0000

    initial commit

---
+++ exercises/progress.js
@@ -0,0 +1,101 @@
+/*
+  Exercise 1
+  JavaScript data structures & functions
+*/
+
+var names = [
+  "Rubin Museum",
+  "The Cooper Hewitt (Smithsonian)",
+  "The Morgan Library and Museum",
+  "The Whitney Museum of American Art",
+  "The Frick Collection",
+  "American Museum of Natural History",
+];
+
+var URLs = [
+  "rubinmuseum.org",
+  "cooperhewitt.org",
+  "themorgan.org",
+  "whitney.org",
+  "frick.org",
+  "amnh.org",
+];
+
+var years = [
+  2004,
+  1896,
+  1924,
+  1930,
+  1935,
+  1869
+];
+
+// Task 1
+// Console log the length of each Array
+console.log("The length of names variable is: " + names.length)
+console.log("The length of URLs variable is: " + URLs.length)
+console.log("The length of years variable is: " + names.length)
+
+// Task 2
+// add a new item to an array
+var newName = "The International Center of Photography"
+var newURL = "icp.org"
+var newYear = 1974
+
+names.push(newName);
+URLs[URLs.length] = newURL;
+years = years.concat(newYear);
+
+// Task 3
+// construct an Object out of our three Arrays
+// the result should look similar to this:
+var result = {
+  "Museum Name 1": {
+    URL: "www.museumwebsite.com",
+    year: 2019
+  }
+}
+
+var museums = {};
+for (var i = 0; i < names.length; i++) {
+  var currentName = names[i];
+  var currentURL = URLs[i];
+  var currentYear = years[i];
+
+  museums[currentName] = {};
+  museums[currentName]["URL"] = currentURL;
+  museums[currentName].year = currentYear;
+}
+
+console.log('museums', museums)
+
+var museums2 = {};
+names.forEach(function(n,i) {
+  museums2[n] = {};
+
+  var currentURL = URLs[i];
+  var currentYear = years[i];
+
+  museums2[n].URL = currentURL;
+  museums2[n]["year"] = currentYear;
+});
+
+console.log('museums2', museums2)
+
+// Task
+// Write a function to add a new museum object, with properties URL and year, to an existing museums object. Call it on museums2
+function addAMuseum(museums, newName, newURL, newYear){
+  
+  var newMuseum = {};
+  
+  newMuseum[newName] = {};
+  newMuseum[newName]["URL"] = newURL;
+  newMuseum[newName]["year"] = newYear;
+
+  museums.push(newMuseum)
+  return museums;
+}
+
+var museums2 = addAMuseum(museums,"The MET", "www.met.org", 2019);
+
+console.log('museums2', museums2);
